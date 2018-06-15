23fca299a829e44b3aa007f6383b017b4e4fdc16
Bakedpork <40089827+Bakedpork@users.noreply.github.com>
2018-06-15 16:11:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,32 +5,19 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'Baketball') {
-    	message.reply('我最愛黑子的籃球了');
-  	}
-});
-client.on('message', message => {
-    if (message.content === 'Lonely') {
-    	message.reply('公主.主人在叫我嗎?');
-  	}
-});
-client.on('message', message => {
-   if (message.content === 'Funny') {
-    	message.reply('我可不覺得.我很寂寞');
-  	}
-}); 
-
-client.on('message', message => {
-   if (message.content === 'XD') {
-    	message.reply('我可不覺得.我很寂寞');
-  	}
-}); 
-client.on('message', message => {
-   if (message.content === 'HAHA') {
-    	message.sendmessage('我可不覺得.我很寂寞');
-  	}
-}); 
+client.on("message", (message) => {
+  if(message.content === "test") {
+    message.channel.send("五分熟機械人");
+  }
+  if(message.content === "Cow") {
+    message.channel.send("我就是五分熟機械人");
+  }
+  if(message.content === "Server Open") {
+    message.channel.send("伺服器已開啟");
+  }
+  if(message.content === "Server close") {
+    message.channel.send("伺服器已關閉");
+  }
     
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
